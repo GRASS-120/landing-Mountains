85feee47c898bcc16a4c6d887eda56d85bd8f363
Lev Khomchenkov <pokemon9116@yandex.ru>
2020-06-10 15:32:36 +0700

    smooth showing navbar during scrolling + burger

--- js/scroll_header.js
+++ js/scroll_header.js
@@ -1,9 +1,12 @@
 window.onscroll = function showHeader () { 
     let header = document.querySelector('.navbar')
+    let header_2 = document.querySelector(".navbar-2")
 
     if (window.pageYOffset > 85) {
-        header.classList.add('navbar_fixed')
+        header.classList.add('navbar_fixed');
+        header_2.classList.add('navbar-2_fixed');
     } else {
-        header.classList.remove('navbar_fixed')
+        header.classList.remove('navbar_fixed');
+        header_2.classList.remove('navbar-2_fixed');
     }
 }
